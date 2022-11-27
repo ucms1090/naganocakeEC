dd476531bd537abc18a7182fc39d96abb6d3f4f4
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-27 21:33:29 +0000

    save

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,9 +22,15 @@ class Admin::ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to admin_items_path
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
 
   private
