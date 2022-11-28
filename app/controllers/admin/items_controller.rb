7acfff8de0f81ded7ac0b34ca84c08f972bca1e2
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-28 09:40:54 +0000

    save-admin

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -36,7 +36,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image)
   end
 
 end
