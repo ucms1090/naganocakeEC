84546f2c5e2cc16658dcbf345f577919df64ee72
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-28 03:28:43 +0000

    [finish]genreselect

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -39,4 +39,7 @@ class Admin::ItemsController < ApplicationController
     params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
   end
 
+  def genre_params
+  end
+
 end
