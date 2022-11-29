6f7eb476ae65b1919bc0b015587c5fafa71795ee
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-29 09:46:33 +0000

    add]hidden_field

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,25 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
   end
+
+  def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.customer_id = current_customer.id
+    cart_item.save
+    redirect_to public_cart_items_path
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def cart_item_params
+    params.permit(:item_id, :customer_id, :amount)
+  end
+
 end
