88327683eaefbf346764326c2f8ed04803b7fbf0
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-29 21:14:03 +0000

    save

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
+    @cart_items = CartItem.where(customer_id: current_customer)
   end
 
   def create
