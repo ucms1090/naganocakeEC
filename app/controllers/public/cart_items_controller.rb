4e971a6d7cdd5b48d95d28d7d9dc479cefaa380e
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-11-30 00:05:55 +0000

    save

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,9 +11,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   private
