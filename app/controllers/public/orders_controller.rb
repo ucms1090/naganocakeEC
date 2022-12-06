3b46a332ad5f54efbdb1d0823d71c20a39e8c9bf
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-12-06 10:03:56 +0000

    save

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,30 @@ class Public::OrdersController < ApplicationController
     @customer = Customer.find(current_customer.id)
   end
 
+  def create
+    binding.pry
+    order = Order.new(order_params)
+    order.customer_id = current_customer.id
+    order.save
+    redirect_to confirm
+  end
+
   def confirm
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.postal_address = current_customer.address
+      @order.postal_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address]  == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.postal_address = @address.address
+      @order.postal_name = @address.name
+    else
+      # select_address == 2
+    end
+    @cart_items = CartItem.where(customer_id: current_customer)
+    @total = 0
   end
 
   def complete
@@ -15,4 +38,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:method_payment, :postal_code, :postal_address, :postal_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   scope module: 'public' do
     resources :items, only: [:show, :index]
     resources :cart_items, only: [:index, :update, :create, :destroy]
-    resources :orders, only: [:new, :show, :index]
+    resources :orders, only: [:new, :show, :index, :create]
+    resources :addresses, only: [:index, :edit, :update, :create, :destroy]
     post 'orders/confirm', to: 'orders#confirm', as: 'orders_confirm'
     get 'orders/complete', to: 'orders#complete', as: 'orders_complete'
     delete 'cart_items/destroy_all', to: 'cart_items#destroy_all', as: 'cart_items_destroy_all'
