f218ecf184985456327b7769ea5384a58ce3b976
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-12-15 21:00:12 +0000

    layout-save

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,5 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
     @total = 0
@@ -6,15 +7,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.item_id = cart_item_params[:item_id]
-    if CartItem.find_by(item_id: cart_item.item_id).present?
-      cart_item.amount = cart_item.amount + params[:amount].to_i
-      cart_item.update(cart_item_params)
+    @cart_item = CartItem.new(cart_item_params)
+    current_cart_items = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if current_cart_items.present?
+      cart_item = CartItem.find_by(item_id: @cart_item.item_id, customer_id: current_customer.id)
+      cart_item.update(amount: cart_item.amount + @cart_item.amount)
       redirect_to cart_items_path
     else
-      cart_item.save
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
       redirect_to cart_items_path
     end
   end
@@ -40,7 +41,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.permit(:item_id, :customer_id, :amount)
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,12 @@
 class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
+    @genres = Genre.all
+    @cart_item = CartItem.new
   end
 
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 end
