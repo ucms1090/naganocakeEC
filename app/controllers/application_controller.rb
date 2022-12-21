9a0da1a04a4f054da41746a582df0b0e063a14c4
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-12-21 03:44:34 +0000

    add]test-admin_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
 
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
