fd7948915e0f384a3ad0285c8c82e26d7d5afff2
ucms1090 <117441108+ucms1090@users.noreply.github.com>
2022-12-26 00:30:18 +0000

    finish

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,6 @@
 class Admin::OrdersController < ApplicationController
   def index
     @orders = Order.all
-    @orders.each do |order|
-      @order_details = OrderDetail.where(order_id: order.id)
-      order_details_item_amount = 0
-      @order_details.each do |order_detail|
-        @order_details_item_amount = order_details_item_amount + order_detail.items_amount
-      end
-    end
   end
 
   def show
